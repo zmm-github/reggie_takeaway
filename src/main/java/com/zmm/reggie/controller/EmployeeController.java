bbfb7169d16856f502909dc947b52e48395d8d75
zmm-github <1519646439@qq.com>
2024-11-04 18:54:30 +0800

    实现员工管理的增删改查

--- src/main/java/com/zmm/reggie/controller/EmployeeController.java
+++ src/main/java/com/zmm/reggie/controller/EmployeeController.java
@@ -1,9 +1,14 @@
 package com.zmm.reggie.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.zmm.reggie.entity.Employee;
+import com.zmm.reggie.service.IEmployeeService;
+import com.zmm.reggie.vo.ResultVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +21,91 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
+
+    @Autowired
+    private IEmployeeService employeeService;
+
+    /**
+     * 通过用户名查找指定员工
+     *
+     * @param username
+     * @return
+     */
+    @GetMapping("/findEmployeeByUsername")
+    public ResultVo<Employee> findEmployeeByUsername(@RequestParam("username") String username) {
+        return employeeService.findEmployeeByUsername(username);
+    }
+
+    /**
+     * 根据token获取当前员工信息
+     *
+     * @param token
+     * @return
+     */
+    @GetMapping("/currentEmployeeInfo")
+    public ResultVo<Employee> findEmployeeByToken(@RequestHeader("authorization") String token) {
+        return employeeService.findEmployeeByToken(token);
+    }
+
+    /**
+     * 查询所有员工列表
+     * @return
+     */
+    @GetMapping("/findEmployees")
+    public ResultVo<List<Employee>> findEmployees() {
+        return employeeService.findEmployees();
+    }
+
+    /**
+     * 根据员工名模糊查询员工信息
+     * @param nickName
+     * @return
+     */
+    @GetMapping("/likeSearchEmployeeByName")
+    public ResultVo<List<Employee>> likeSearchEmployeeByName(@RequestParam(required = false, value = "nickName") String nickName) {
+        return employeeService.likeSearchEmployeeByName(nickName);
+    }
+
+    /**
+     * 新增员工
+     * @param employee
+     * @param token
+     * @return
+     */
+    @PostMapping("/addEmployee")
+    public ResultVo addEmployee(@RequestBody Employee employee, @RequestHeader("authorization") String token) {
+        return employeeService.addEmployee(employee, token);
+    }
+
+    /**
+     * 根据员工ID删除员工信息
+     * @param id
+     * @return
+     */
+    @DeleteMapping("/delEmployeeById/{id}")
+    public ResultVo delEmployeeById(@PathVariable Integer id) {
+        return employeeService.delEmployeeById(id);
+    }
+
+    /**
+     * 根据员工ID更新员工信息
+     * @param employee
+     * @param token
+     * @return
+     */
+    @PutMapping("/updateEmployeeById")
+    public ResultVo updateEmployeeById(@RequestBody Employee employee, @RequestHeader("authorization") String token) {
+        return employeeService.updateEmployeeById(employee, token);
+    }
+
+    /**
+     * 获取当前登录员工的角色列表
+     * @param employeeId
+     * @return
+     */
+    @GetMapping("/getLoginEmployeeRoles")
+    public ResultVo getLoginEmployeeRoles(Integer employeeId) {
+        return employeeService.getLoginEmployeeRoles(employeeId);
+    }
 
 }
--- src/main/java/com/zmm/reggie/entity/Employee.java
+++ src/main/java/com/zmm/reggie/entity/Employee.java
@@ -1,15 +1,17 @@
 package com.zmm.reggie.entity;
 
-import com.baomidou.mybatisplus.annotation.TableName;
-import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.*;
+
 import java.time.LocalDateTime;
-import com.baomidou.mybatisplus.annotation.TableId;
 import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+import org.springframework.format.annotation.DateTimeFormat;
 
 /**
  * <p>
@@ -17,7 +19,7 @@ import lombok.experimental.Accessors;
  * </p>
  *
  * @author zmm
- * @since 2024-09-24
+ * @since 2024-10-21
  */
 @Data
 @EqualsAndHashCode(callSuper = false)
@@ -32,8 +34,8 @@ public class Employee implements Serializable {
     @TableId(value = "id", type = IdType.AUTO)
     private Long id;
 
-    @ApiModelProperty(value = "姓名")
-    private String name;
+    @ApiModelProperty(value = "昵称")
+    private String nickName;
 
     @ApiModelProperty(value = "用户名")
     private String username;
@@ -44,26 +46,33 @@ public class Employee implements Serializable {
     @ApiModelProperty(value = "手机号")
     private String phone;
 
-    @ApiModelProperty(value = "性别")
-    private String sex;
+    @ApiModelProperty(value = "性别 0：男，1：女")
+    private Integer sex;
 
     @ApiModelProperty(value = "身份证号")
     private String idNumber;
 
-    @ApiModelProperty(value = "状态 0:禁用，1:正常")
+    @ApiModelProperty(value = "状态 0：禁用，1：正常")
     private Integer status;
 
     @ApiModelProperty(value = "创建时间")
+    @TableField(fill = FieldFill.INSERT) // 插入时填充字段
     private LocalDateTime createTime;
 
     @ApiModelProperty(value = "更新时间")
+    @TableField(fill = FieldFill.INSERT_UPDATE) // 插入和更新时填充字段
     private LocalDateTime updateTime;
 
     @ApiModelProperty(value = "创建人")
+    @TableField(fill = FieldFill.INSERT) // 插入时填充字段
     private Long createUser;
 
     @ApiModelProperty(value = "修改人")
+    @TableField(fill = FieldFill.INSERT_UPDATE) // 插入和更新时填充字段
     private Long updateUser;
+
+    @ApiModelProperty(value = "员工头像")
+    private String avatar;
 
 
 }
